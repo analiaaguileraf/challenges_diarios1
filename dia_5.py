7b263b07fcf357a61e4a36b1afa7d90f31d4d2d6
analiaaguileraf <155084072+analiaaguileraf@users.noreply.github.com>
2024-06-17 15:55:41 -0400

    Add files via upload

---
+++ dia_5.py
@@ -0,0 +1,15 @@
+
+# Challenge ---> Crear un Diccionario: Escribe un programa que cree un diccionario a partir de dos listas dadas: una de claves y otra de valores.
+
+
+claves_us = input("ingrese palabra clave:")
+valores_us = input("ingrese un valor:")
+
+claves = claves_us.split()
+valores = valores_us.split()
+
+diccionario = {}
+for i in range (len(claves)):
+    diccionario[claves[i]] = valores [i]
+
+print ("Diccionario creado:", diccionario)
